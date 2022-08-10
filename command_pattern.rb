a34905b49e8e24aa461d2983f17ab5e1f79be9fb
Ahmad Bakry <ahmadbakry.gad@gmail.com>
2022-08-10 02:57:59 +0200

    Comment the description of the Command Pattern

--- command_pattern.rb
+++ command_pattern.rb
@@ -1,5 +1,5 @@
-Assuming that we are building a GUI framework that has so many buttons for example doing so many 
-function.And we have our Button class like below:
+# Assuming that we are building a GUI framework that has so many buttons for example doing so many 
+# function.And we have our Button class like below:
 
 class Button
   # tens of lines related to the 
@@ -10,10 +10,10 @@ class Button
   end
 end
 
-* our button class will be instantiated and thousands of instances of the same button will be created.
-  And each will have a specific task to do according to the business.
+# * our button class will be instantiated and thousands of instances of the same button will be created.
+#   And each will have a specific task to do according to the business.
 
-* One dummy solution is to use inheritance and create a button for each task like below:
+# * One dummy solution is to use inheritance and create a button for each task like below:
 
 class SaveButton < Button 
   def on_push_button
@@ -27,26 +27,26 @@ class NewDocumentButton < Button
   end
 end
 
-This solution is dummy, because we may need hundreds of diffrent buttons.
+# This solution is dummy, because we may need hundreds of diffrent buttons.
 
 # -------------------------------------------------------------------------
 
 # An easier way to do that:
 # ==========================
 
-We need to remember one the most important principles mentioned before:
+# We need to remember one the most important principles mentioned before:
 
-"Separate out the things that change from those that stay the same."
+# "Separate out the things that change from those that stay the same."
 
-we want to bundle up the code to handle
-the button push or menu selection in its own object—an object that does nothing but
-wait to be executed and, when executed, goes out and performs an application-specific
-task.
+# we want to bundle up the code to handle
+# the button push or menu selection in its own object—an object that does nothing but
+# wait to be executed and, when executed, goes out and performs an application-specific
+# task.
 
-These packages of action are the commands of the command pattern.
+# These packages of action are the commands of the command pattern.
 
-To apply the Command pattern to our button example, we simply store a com-
-mand object with each button.
+# To apply the Command pattern to our button example, we simply store a com-
+# mand object with each button.
 
 class Button
   attr_accessor :command 
@@ -68,18 +68,18 @@ end
 
 save_button = Button.new(SaveCommand.new)
 
-The connection between the button and the command is a runtime connection, so the button needs to have a
-reference to the command.And, we can change the command on the fly and change the bahavior or function
-consequently.
+# The connection between the button and the command is a runtime connection, so the button needs to have a
+# reference to the command.And, we can change the command on the fly and change the bahavior or function
+# consequently.
 
 # ----------------------------------------------------
 
 # code blocks as commands.
 # ==========================
 
-It is clear now that commands are only wrappers around some code that has onw thing to do 
-and the only reason for its existence is to run code at the right time.
-This is the difinition of code block.
+# It is clear now that commands are only wrappers around some code that has onw thing to do 
+# and the only reason for its existence is to run code at the right time.
+# This is the difinition of code block.
 
 class Button
   attr_accessor :command 
@@ -106,10 +106,10 @@ end
 # =========================
 # Command pattern can be useful in keeping track of what you have already done.
 
-Assuming that we are building an installtion program that installs soe code to your device.
-This installation program may have some tasks like, creating new files, copying them or even deleting some files.
-And they need to tell the user about what is the current running procrss or comand or even what has been done.
-It is better here to have our command interface class with a description about the command.
+# Assuming that we are building an installtion program that installs soe code to your device.
+# This installation program may have some tasks like, creating new files, copying them or even deleting some files.
+# And they need to tell the user about what is the current running procrss or comand or even what has been done.
+# It is better here to have our command interface class with a description about the command.
 
 class Command 
   attr_reader :description
@@ -159,9 +159,9 @@ def CopyFileCommand < Command
   end
 end
 
-Here we are interested in tracking what we are doing during , before or even after the software installation.
-We need to wrap all the previous command in an interface that looks like a command and has 
-sub commands underneath.It looks like we a composite of command 
+# Here we are interested in tracking what we are doing during , before or even after the software installation.
+# We need to wrap all the previous command in an interface that looks like a command and has 
+# sub commands underneath.It looks like we a composite of command.
 
 class CompositeCommand < Command 
   def initialize
@@ -189,27 +189,27 @@ cmds.add_command(CopyFileCommand.new(file.txt, file2.txt))
 cmds.add_command(DeleteFileCommand.new(file.txt))
 cmds.excute
 
-Here we can get the description of all the installation process by:
+# Here we can get the description of all the installation process by:
 
 puts cmds.description
 
-Also, we can get the description at any moment of the installation process, 
-by asking each excute command to print its description.
+# Also, we can get the description at any moment of the installation process, 
+# by asking each excute command to print its description.
 
 # --------------------------------------------------------
 
 # Getting Things Done with Commands
 
-Undoing changes done by human or even software is a necissity nowadays.
-Undoing changes for example can be found in so many applications like the text editors for example.
-All database transaction can be rolled back.This is a sort of undoing.
-Undoing some change can be done by keeping track of the state of the object in the context 
-of change before and after the change.This can be a tedious and dummy operation in case of file having thousands of lines.
+# Undoing changes done by human or even software is a necissity nowadays.
+# Undoing changes for example can be found in so many applications like the text editors for example.
+# All database transaction can be rolled back.This is a sort of undoing.
+# Undoing some change can be done by keeping track of the state of the object in the context 
+# of change before and after the change.This can be a tedious and dummy operation in case of file having thousands of lines.
 
-We can use the command pattern to simply represent the undo operation, since it is simply a command and 
-it is the reverse operation of the excution of a command.
-so, for each command having excute method, we cam implement the unexcute method too.
-In the case of software installation example, we can implement like so:
+# We can use the command pattern to simply represent the undo operation, since it is simply a command and 
+# it is the reverse operation of the excution of a command.
+# so, for each command having excute method, we cam implement the unexcute method too.
+# In the case of software installation example, we can implement like so:
 
 def NewFileCommand < Command
   def initialize(path, contents)
@@ -272,7 +272,7 @@ def CopyFileCommand < Command
   end
 end
 
-also unexcute can be implemented in the composite command like below:
+# also unexcute can be implemented in the composite command like below:
 
 class CompositeCommand < Command 
   def initialize
@@ -297,6 +297,11 @@ class CompositeCommand < Command
     @commands.reverse.each { |c| c.unexcute }
   end
 end
+
+# "The key thing about the Command pattern is that it separates the thought from
+# the deed. When you use this pattern, you are no longer simply saying, “Do this”;
+# instead, you are saying, “Remember how to do this,” and, sometime later, “Do that
+# thing that I told you to remember.” "
 
 
 
